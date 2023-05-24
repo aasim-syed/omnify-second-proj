a325cb40e834e00980ef7ab5c13b897e034a6983
Syed Aasim <syedaasim133@gmail.com>
2023-05-24 07:13:38 +0530

    latest refactor v1.0

--- src/utils/apiInfo.js
+++ src/utils/apiInfo.js
@@ -1,3 +1,3 @@
-export const apiKey = process.env.REACT_APP_ACCESS_KEY;
+export const apiKey = "fbbfe22a1c4c83e9bc032cf172038e94";
 
 export const BASE_URL = "https://api.openweathermap.org/data/2.5";
